fe1a002dbce991b5d33acb6ac41f008e299737ba
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-29 22:35:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+const arr = [1, 2, 3, 4];
+const odd = []
+const mult = []
+
+function getData(){
+	return new Promise((resolve, reject) => {
+		setTime(() => {
+			resolve([1,2,3,4]);
+		}, 3000);
+	});	
+}
+
+getData()
+	.then((numbers) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const evenNumbers = numbers.filter((num) => num % 2 === 0);
+				output.textContent = evenNumbers.join(", ");
+				resolve(evenNumbers);
+			}, 1000);
+		});
+	})
+	.then((evenNumbers) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const multipliedNumbers = evenNumbers.filter((num) => num * 2)
+				output.textContent = multipliedNumbers.join(", ");
+				resolve(multipliedNumbers);
+			}, 2000);
+		});
+	})
+	.catch((error) => consloe.eror(error));
+
+
+
+
